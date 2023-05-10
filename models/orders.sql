d185f538d7f5d1c95b81281bdf1a8f2817a15c2c
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-05-10 18:46:37 +0000

    🔁 Modified     - Model        - orders        - stg_customers        - customers        - stg_payments        - stg_orders
    ❇️ New
        - Source
           - raw_test_source
           - raw_received
        - Seed
           - raw_shipments

--- models/orders.sql
+++ models/orders.sql
@@ -53,8 +53,36 @@ final AS (
   LEFT JOIN order_payments
      ON orders.order_id = order_payments.order_id
 
+),
+
+topOrders AS (
+
+  SELECT * 
+  
+  FROM final AS in0
+  
+  WHERE true
+
+),
+
+Reformat_1 AS (
+
+  SELECT 
+    order_id AS order_id,
+    customer_id AS customer_id,
+    order_date AS order_date,
+    status AS status,
+    credit_card_amount AS credit_card_amount,
+    coupon_amount AS coupon_amount,
+    bank_transfer_amount AS bank_transfer_amount,
+    gift_card_amount AS gift_card_amount,
+    amount AS amount,
+    concat(order_id, " ", status) AS id_status
+  
+  FROM topOrders AS in0
+
 )
 
 SELECT *
 
-FROM final
+FROM Reformat_1
